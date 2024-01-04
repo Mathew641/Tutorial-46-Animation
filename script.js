7921ab1b9af542879ebbe60962c624ea8d2f71e1
Mathew641 <106030476+Mathew641@users.noreply.github.com>
2024-01-04 03:22:15 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+// alert("Welcome to the Tutorial-46 of sigma web development class about the Animation in CSS");
+
+console.log("Welcome to the tutorial 46 about the animation in css of sigma web development!!!!");
